0462dd8cffe5326ffcdd9a3913fdebfaad76a919
wommy <tom@technomad.media>
2022-01-11 17:28:43 -0500

    feat-11: passthru nCMS

--- packages/eleventy/.eleventy.js
+++ packages/eleventy/.eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig){
 	eleventyConfig.ignores.add('README.md')
 	eleventyConfig.addPassthroughCopy({'../tailwind/style/': 'style/'})
+	eleventyConfig.addPassthroughCopy({'../netlifyCMS/admin': 'admin/'})
 	return {
 		dataTemplateEngine: 'njk',
 		markdownTemplateEngine: 'njk',
