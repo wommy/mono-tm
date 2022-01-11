1589d1074ee12227bab10b14eca23ca999af2d1c
wommy <tom@technomad.media>
2022-01-11 16:54:10 -0500

    up-11: config, base, reset.min

--- packages/eleventy/.eleventy.js
+++ packages/eleventy/.eleventy.js
@@ -1,4 +1,4 @@
 module.exports = function(c) {
 	c.ignores.add('README.md')
-	c.addPassthroughCopy({'../tailwind/style.css': 'style.css'})
+	c.addPassthroughCopy({'../tailwind/style/': 'style/'})
 }
