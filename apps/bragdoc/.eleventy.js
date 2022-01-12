7d01ef20013505dfbf83eb0f4de097016b87752b
wommy <tom@technomad.media>
2022-01-12 18:09:25 -0500

    clean-11: cp config

--- apps/bragdoc/.eleventy.js
+++ apps/bragdoc/.eleventy.js
@@ -1,7 +1,22 @@
-module.exports = function(c) {
-	c.ignores.add('README.md')
-	c.addPassthroughCopy("admin")
+const { DateTime } = require('luxon')
+
+module.exports = function(eleventyConfig){
+	eleventyConfig.ignores.add('README.md')
+	
+	eleventyConfig.addPassthroughCopy({'../../packages/tailwind/style/': 'style/'})
+	eleventyConfig.addPassthroughCopy({'../../packages/netlifyCMS/admin': 'admin/'})
+
+	eleventyConfig.addFilter('readableDate', dateObj => {
+		return DateTime.fromJSDate(dateObj).toFormat('dd LLL yyyy')
+	})
+	eleventyConfig.addFilter('htmlDateString', dateObj => {
+		return DateTime.fromJSDate(dateObj).toFormat('yyyy-LL-dd')
+	})
+
 	return {
+		dataTemplateEngine: 'njk',
+		markdownTemplateEngine: 'njk',
+		htmlTemplateEngine: 'njk',
 		dir: {
 			includes: "../../packages/eleventy/_includes"
 		}
