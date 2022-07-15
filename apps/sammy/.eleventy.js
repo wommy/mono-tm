c36d99a8a13d4c71e90324c83254816c37079aad
wommy <tom@technomad.media>
2022-07-15 03:19:04 -0400

    init: sammy

---
+++ apps/sammy/.eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig){
+	eleventyConfig.ignores.add('README.md')
+	
+	// eleventyConfig.addPassthroughCopy({'../tailwind/style/': 'style/'})
+
+	return {
+		dataTemplateEngine: 'njk',
+		markdownTemplateEngine: 'njk',
+		htmlTemplateEngine: 'njk',
+	}
+}
